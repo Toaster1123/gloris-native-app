8bee2b114caa6cb5c8d7db983af9d0d95453febf
Toaster1123 <prokhorov.rostislav@gmail.com>
2025-07-19 23:29:39 +0300

    feat(config): config for build

--- app.config.js
+++ app.config.js
@@ -1,17 +1,15 @@
 import 'dotenv/config';
 
-export default {
-  expo: {
-    name: 'Gloris',
-    slug: 'gloris-app',
-    version: '1.0.0',
-    android: {
-      package: 'com.gloris.scheduleapp',
-    },
+export default ({ config }) => {
+  return {
+    ...config,
     extra: {
+      ...config.extra,
       SCHEDULE_URL: process.env.SCHEDULE_URL,
       URL_KEY: process.env.URL_KEY,
+      eas: {
+        projectId: '3605cb9c-c8a8-4146-86b8-79a432d81b23',
+      },
     },
-    plugins: [['expo-build-properties']],
-  },
+  };
 };
